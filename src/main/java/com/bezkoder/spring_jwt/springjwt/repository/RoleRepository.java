fdfcd94e6c3cef1cbec9f33d15a8f0ff6acc90ea
amalabms <56789725+amalabms@users.noreply.github.com>
2021-08-04 17:24:08 +0100

    first commit

---
+++ src/main/java/com/bezkoder/spring_jwt/springjwt/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.bezkoder.spring_jwt.springjwt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bezkoder.spring_jwt.springjwt.models.ERole;
+import com.bezkoder.spring_jwt.springjwt.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(ERole name);
+}
